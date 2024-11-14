9b63bd2a7be6ab77c3dded491b900ebc45841fc5
Sebastian Kotarski <>
2024-11-14 22:31:20 +0100

    Fine tune

--- ARChitect/Services/EntityService.swift
+++ ARChitect/Services/EntityService.swift
@@ -3,12 +3,13 @@ import Foundation
 
 final class EntityService: ObservableObject {
     private let entityRepository: EntityRepository
+    private let suffix = ", white background"
     
     init(entityRepository: EntityRepository) {
         self.entityRepository = entityRepository
     }
     
     func fetchEntity(input: String) async throws -> Entity? {
-        try await entityRepository.fetchEntity(input: input)
+        try await entityRepository.fetchEntity(input: input.appending(suffix))
     }
 }
